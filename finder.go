cf2c60d8673c603028107cb17c9ae5f2e853b791
bigcubecat <mr.bitochkin@yandex.ru>
2021-02-05 17:54:00 +0700

    5D

--- finder.go
+++ finder.go
@@ -24,55 +24,55 @@ func generateS(b int, d int) []int {
 }
 
 func findRules(size int) {
-	for D := 4; D < 11; D++ {
-		var b []int
-		var s []int
-		v := countNeighbours(D)
-		q := v / 4
-		for b_variant := q - D; b_variant <= q+D; b_variant++ {
-			if b_variant <= 0 {
-				continue
+	D := 5
+	var b []int
+	var s []int
+	v := countNeighbours(D)
+	q := v / 4
+	for b_variant := q - D + 1; b_variant <= q+D-1; b_variant++ {
+		if b_variant <= 0 {
+			continue
+		}
+		b = append(b, b_variant)
+		s = generateS(b_variant, D)
+		for t := 0; t < 100; t++ {
+			life := Life{N: D, SIZE: size}
+			var d []bool
+
+			d = make([]bool, intPow(size, D))
+			fmt.Print(len(d))
+			for x := range d {
+				d[x] = random() && random()
 			}
-			b = append(b, b_variant)
-			s = generateS(b_variant, D)
-			for t := 0; t < 100; t++ {
-				life := Life{N: D, SIZE: size}
-				var d []bool
+			life.Setup(b, s, d)
+			fmt.Println("Rules = ", b_variant)
 
-				d = make([]bool, intPow(size, D))
-				fmt.Print(len(d))
-				for x := range d {
-					d[x] = random() && random()
-				}
-				life.Setup(b, s, d)
-				fmt.Println(life.dataSize)
-
-				fmt.Println("Starting Evolution")
-				for g := 0; g < 10; g++ {
-					life.NextGeneration()
-				}
-				b_string := strconv.Itoa(b_variant)
-				var s_string []string
-				for _, value := range s {
-					s_string = append(s_string, strconv.Itoa(value))
-				}
-				out := RLECode(dataToString(life.GetData()))
-				fmt.Println("Starting write to file")
-				output := dataModel{
-					D:    life.N,
-					SIZE: life.SIZE,
-					DATA: out,
-					B:    b_string,
-					S:    strings.Join(s_string, ","),
-				}
-				file, _ := json.MarshalIndent(output, "", "")
-				outputFile := "data" + strconv.Itoa(D) + "B" + strconv.Itoa(b_variant) + "try" + strconv.Itoa(t) + ".json"
-				saveErr := ioutil.WriteFile(outputFile, file, 0644)
-				if saveErr != nil {
-					panic(saveErr)
-				} else {
-					fmt.Println("Finish write to file. No errors")
-				}
+			fmt.Println("Starting Evolution")
+			for g := 0; g < 10; g++ {
+				life.NextGeneration()
+			}
+			fmt.Println("Start coding")
+			b_string := strconv.Itoa(b_variant)
+			var s_string []string
+			for _, value := range s {
+				s_string = append(s_string, strconv.Itoa(value))
+			}
+			out := RLECode(dataToString(life.GetData()))
+			fmt.Println("Starting write to file")
+			output := dataModel{
+				D:    life.N,
+				SIZE: life.SIZE,
+				DATA: out,
+				B:    b_string,
+				S:    strings.Join(s_string, ","),
+			}
+			file, _ := json.MarshalIndent(output, "", "")
+			outputFile := "data" + strconv.Itoa(D) + "B" + strconv.Itoa(b_variant) + "try" + strconv.Itoa(t) + ".json"
+			saveErr := ioutil.WriteFile(outputFile, file, 0644)
+			if saveErr != nil {
+				panic(saveErr)
+			} else {
+				fmt.Println("Finish write to file. No errors")
 			}
 		}
 	}
