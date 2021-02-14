4e78a6950a2c64661290a6f86e33607b9b918f73
bigcubecat <mr.bitochkin@yandex.ru>
2021-02-14 15:48:20 +0700

    change saving in find mod

--- finder.go
+++ finder.go
@@ -30,32 +30,33 @@ func findRules(size int, D int, G int) {
 		fmt.Println("Starting Evolution")
 		for g := 0; g < G; g++ {
 			life.NextGeneration()
-		}
-		fmt.Println("Start coding")
-		var s_string []string
-		var b_string []string
-		for _, value := range s {
-			s_string = append(s_string, strconv.Itoa(value))
-		}
-		for _, value := range b {
-			b_string = append(b_string, strconv.Itoa(value))
-		}
-		out := RLECode(dataToString(life.GetData()))
-		output := dataModel{
-			D:    life.N,
-			SIZE: life.SIZE,
-			DATA: out,
-			B:    strings.Join(b_string, ","),
-			S:    strings.Join(s_string, ","),
-		}
-		file, _ := json.MarshalIndent(output, "", "")
-		outputFile := "data" + strconv.Itoa(D) + "try" + strconv.Itoa(t) + ".life"
-		fmt.Println("Starting write to file")
-		saveErr := ioutil.WriteFile(outputFile, file, 0644)
-		if saveErr != nil {
-			panic(saveErr)
-		} else {
-			fmt.Println("Finish write to file. No errors")
+			fmt.Println("Start coding")
+			var s_string []string
+			var b_string []string
+			for _, value := range s {
+				s_string = append(s_string, strconv.Itoa(value))
+			}
+			for _, value := range b {
+				b_string = append(b_string, strconv.Itoa(value))
+			}
+			out := RLECode(dataToString(life.GetData()))
+			output := dataModel{
+				D:    life.N,
+				SIZE: life.SIZE,
+				DATA: out,
+				B:    strings.Join(b_string, ","),
+				S:    strings.Join(s_string, ","),
+			}
+			file, _ := json.MarshalIndent(output, "", "")
+			outputFile := "data" + strconv.Itoa(D) + "try" + strconv.Itoa(t) + ".life"
+			fmt.Println("Starting write to file")
+			saveErr := ioutil.WriteFile(outputFile, file, 0644)
+			if saveErr != nil {
+				panic(saveErr)
+			} else {
+				fmt.Println("Finish write to file. No errors")
+			}
+			fmt.Println("End evolution")
 		}
 	}
 }
